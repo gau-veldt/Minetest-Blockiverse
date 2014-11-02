73e5f4a6411d9c9780394cfadc2cec207bb51fbb
gau-veldt <gau_veldt@hotmail.com>
2014-11-01 23:57:29 -0700

    cpp/hpp source templates
    cpp/hpp source template stubs for adding new files to the project

---
+++ blockiverse/blank.hpp
@@ -0,0 +1,18 @@
+/*
+**
+**  Minetest-Blockiverse
+**
+**  Incorporates code from minetest 0.4.10-dev
+**
+**  Blockiverse
+**  Copyright (C) 2014 Brian Jack <gau_veldt@hotmail.com>
+**  Distributed as free software using the copyleft
+**  LGPL Version 3 license:
+**  https://www.gnu.org/licenses/lgpl-3.0.en.html
+**  See file LICENSE in ../
+**
+**  Declaration (header) file {Thing}.hpp
+**
+**  {WhatThingDoes}
+**
+*/
